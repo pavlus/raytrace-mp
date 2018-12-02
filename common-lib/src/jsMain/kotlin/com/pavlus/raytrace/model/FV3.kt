618f91926890197f847e9d9919e7c8b1497498d8
Pavel Remygailo <pavel.remygailo+github@gmail.com>
2018-12-02 23:35:32 +0200

    Cleanup

--- common-lib/src/jsMain/kotlin/com/pavlus/raytrace/model/FV3.kt
+++ common-lib/src/jsMain/kotlin/com/pavlus/raytrace/model/FV3.kt
@@ -1,5 +1,1 @@
 package com.pavlus.raytrace.model
-
-actual data class FV3 actual constructor(actual val x: Float, actual val y: Float, actual val z: Float) {
-    actual constructor(x: Number, y: Number, z: Number) : this(x.toFloat(), y.toFloat(), z.toFloat())
-}
